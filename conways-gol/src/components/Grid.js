9e78bccd06a17ae922cb6a432156080fd64620da
danpatrickoneill <dan47573@gmail.com>
2020-05-26 14:50:49 -0400

    Initial functional comp refactor done

--- conways-gol/src/components/Grid.js
+++ conways-gol/src/components/Grid.js
@@ -1,69 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cell from './Cell';
 import { generateLife } from '../utils/generateLife';
 import { buildLifeMap } from '../utils/buildLifeMap';
 import { liveOn } from '../utils/liveOn';
 
+// triggerEvolution() {
+//   console.log('Evolving!');
+//   this.bufferSwitch = this.bufferSwitch ? false : true;
+//   // console.log(this.state.grid);
+//   // console.log(this.state.gridTwo);
+//   // if (this.bufferSwitch) {
+//   //   this.setState({ grid: liveOn(this.state.gridTwo) });
+//   // } else {
+//   //   this.setState({ gridTwo: liveOn(this.state.grid) });
+//   // }
+//   let temp = liveOn(this.lifeMap);
+//   this.setState({ grid: temp });
+//   this.lifeMap = temp;
+// }
 // Could set a bool inside the class to determine which grid gets shown; flag one as active
-class Grid extends React.Component {
-  constructor() {
-    super();
-    // this.grid = [];
-    // this.bufferGrid = [];
-    this.dimension = 10;
-    this.lifeMap = generateLife(this.dimension);
-    this.state = {
-      grid: buildLifeMap(this.dimension, this.lifeMap),
-      gridTwo: liveOn(this.lifeMap),
-    };
-    this.bufferSwitch = true;
-  }
-
-  triggerEvolution() {
-    console.log('Evolving!');
-    this.bufferSwitch = this.bufferSwitch ? false : true;
-    // console.log(this.state.grid);
-    // console.log(this.state.gridTwo);
-    // if (this.bufferSwitch) {
-    //   this.setState({ grid: liveOn(this.state.gridTwo) });
-    // } else {
-    //   this.setState({ gridTwo: liveOn(this.state.grid) });
-    // }
-    let temp = liveOn(this.lifeMap);
-    this.setState({ grid: temp });
-    this.lifeMap = temp;
-  }
-  render() {
-    // console.log(this.lifeMap);
-    return (
+const Grid = () => {
+  // constructor() {
+  //   super();
+  //   // this.grid = [];
+  //   // this.bufferGrid = [];
+  //   this.dimension = 10;
+  //   this.lifeMap = generateLife(this.dimension);
+  //   this.state = {
+  //     grid: buildLifeMap(this.dimension, this.lifeMap),
+  //     gridTwo: liveOn(this.lifeMap),
+  //   };
+  //   this.bufferSwitch = true;
+  // }
+  const [grid, setGrid] = useState([
+    [true, true, true],
+    [false, false, false],
+    [true, true, true],
+  ]);
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.triggerEvolution()}>
-            Live and Let Die
-          </button>
+        <button>Live and Let Die</button>
 
-          <button
-            onClick={() =>
-              this.setState({
-                grid: buildLifeMap(this.dimension, this.lifeMap),
-              })
-            }
-          >
-            Random Initialize
-          </button>
-        </div>
-        {this.state.grid.map((row) => {
-          return (
-            <div>
-              {row.map((life) => {
-                return <Cell alive={life} />;
-              })}
-            </div>
-          );
-        })}
+        <button>Random Initialize</button>
       </div>
-    );
-  }
-}
+      {grid.map((row) => {
+        return (
+          <div>
+            {row.map((life) => {
+              return <Cell alive={life} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Grid;
