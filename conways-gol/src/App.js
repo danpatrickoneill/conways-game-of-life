d797f0fc3211ab71af7c1bb9dd8e6fc2ed0e104a
danpatrickoneill <dan47573@gmail.com>
2020-05-26 11:35:41 -0400

    Grid and second grid displaying; logic to come

--- conways-gol/src/App.js
+++ conways-gol/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>Canvas Practice!</header>
+      {/* <header className='App-header'>Canvas Practice!</header> */}
       <Grid />
     </div>
   );
--- conways-gol/src/components/Cell.js
+++ conways-gol/src/components/Cell.js
@@ -1,16 +1,28 @@
 import React, { createRef } from 'react';
 
+// The plan; use Cells made up of a single canvas element to respond to props being passed down from the grid;
 class Cell extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
   }
 
+  componentDidMount() {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    if (this.props.alive) {
+      ctx.fillStyle = '#008000';
+    } else {
+      ctx.fillStyle = '#A9A9A9';
+    }
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
   toggleLife = (e) => {
     const canvas = this.canvasRef.current;
     const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FFFFFF';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    window.alert('Hey!');
   };
 
   render() {
@@ -19,7 +31,7 @@ class Cell extends React.Component {
         ref={this.canvasRef}
         width='200'
         height='200'
-        onClick={this.toggleLife}
+        onClick={() => window.alert(this.props.alive)}
       ></canvas>
     );
   }
