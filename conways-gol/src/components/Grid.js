84a3c094b5c2325466bcda846a81019ef667efec
danpatrickoneill <dan47573@gmail.com>
2020-05-27 19:45:33 -0400

    Added gen count; changed grid size; still bug on first advance

--- conways-gol/src/components/Grid.js
+++ conways-gol/src/components/Grid.js
@@ -9,8 +9,7 @@ const Grid = () => {
   const [nextGrid, setNextGrid] = useState([]);
   const [lifeMap, setLifeMap] = useState(null);
   const [swapped, setSwapped] = useState(false);
-
-  function simulate() {}
+  const [generation, setGeneration] = useState(1);
 
   function advanceTime() {
     setSwapped(!swapped);
@@ -19,54 +18,64 @@ const Grid = () => {
     } else {
       setNextGrid(liveOn(grid));
     }
+    setGeneration(generation + 1);
   }
 
   function buffer() {
     setNextGrid(liveOn(grid));
   }
 
-  async function initialize() {
-    setLifeMap(generateLife(100));
-    setTimeout(() => {
-      setGrid(buildLifeMap(lifeMap));
-    }, 8000);
-  }
+  // async function initialize() {
+  //   setLifeMap(generateLife(50));
+  //   setTimeout(() => {
+  //     setGrid(buildLifeMap(lifeMap));
+  //   }, 8000);
+  // }
 
   let key = 0;
   return (
     <div>
       <div>
-        <button onClick={() => simulate()}>Run Simulation</button>
         <button onClick={() => advanceTime()}>Live and Let Die</button>
         <button onClick={() => buffer()}>Buffer</button>
         <button onClick={() => setGrid(buildLifeMap(lifeMap))}>
           Random Initialize
         </button>
-        <button onClick={() => setLifeMap(generateLife(100))}>
+        <button onClick={() => setLifeMap(generateLife(60))}>
           Generate Lifemap
         </button>
+        <div>
+          <p style={{ margin: '1px' }}>Current Generation: {generation}</p>
+        </div>
       </div>
-      {swapped
-        ? nextGrid.map((row) => {
-            return (
-              <div style={{ height: '10px' }}>
-                {row.map((life) => {
-                  key++;
-                  return <Cell key={key} alive={life} />;
-                })}
-              </div>
-            );
-          })
-        : grid.map((row) => {
-            return (
-              <div style={{ height: '10px' }}>
-                {row.map((life) => {
-                  key++;
-                  return <Cell key={key} alive={life} />;
-                })}
-              </div>
-            );
-          })}
+      {
+        <>
+          <div style={{ display: swapped ? 'block' : 'none' }}>
+            {grid.map((row) => {
+              return (
+                <div style={{ height: '5px' }}>
+                  {row.map((life) => {
+                    key++;
+                    return <Cell key={key} alive={life} />;
+                  })}
+                </div>
+              );
+            })}
+          </div>
+          <div style={{ display: swapped ? 'none' : 'block' }}>
+            {nextGrid.map((row) => {
+              return (
+                <div style={{ height: '5px' }}>
+                  {row.map((life) => {
+                    key++;
+                    return <Cell key={key} alive={life} />;
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      }
     </div>
   );
 };
